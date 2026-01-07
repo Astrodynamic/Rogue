d29732179d92cfe06b455e3f2df2712663096e4d
Astrodynamic <Astrodynamic.fkg@gmail.com>
2026-01-07 17:59:39 +0800

    #11 - simple sanitize name

--- internal/adapters/storage/utils.go
+++ internal/adapters/storage/utils.go
@@ -1,22 +1,10 @@
 package storage
 
-import (
-	"strings"
-	"unicode"
-)
+import "strings"
 
 func sanitizePlayerName(name string) string {
-	var result strings.Builder
-	for _, r := range name {
-		if unicode.IsLetter(r) || unicode.IsNumber(r) || r == '_' || r == '-' {
-			result.WriteRune(r)
-		} else if unicode.IsSpace(r) {
-			result.WriteRune('_')
-		}
-	}
-	sanitized := result.String()
-	if sanitized == "" {
+	if name == "" {
 		return "Player"
 	}
-	return sanitized
+	return strings.ReplaceAll(name, " ", "_")
 }
