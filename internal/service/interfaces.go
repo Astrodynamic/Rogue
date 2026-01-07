91141dd2d94a306bcdda6a1e84fd0addcdff9b1f
Astrodynamic <Astrodynamic.fkg@gmail.com>
2026-01-07 20:21:46 +0800

    #11 - remove storage_interface

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -51,3 +51,15 @@ type UI interface {
 	ProcessMenuSelection(hasSave bool) Command
 	ReturnToMenu()
 }
+
+type StateStore interface {
+	Save(world *domain.World) error
+	Load(playerName string) (*domain.World, error)
+	HasSave(playerName string) bool
+	ListSaves() ([]string, error)
+}
+
+type StatsStore interface {
+	SavePlaythrough(stats *domain.PlayStats) error
+	GetLeaderboard(limit int) ([]*domain.PlayStats, error)
+}
